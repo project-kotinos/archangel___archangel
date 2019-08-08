30de05c1359370d9c02a9cfaaac8e2cbf61aab7f
wmuenigneer <adam.d.bell@icloud.io>
2019-08-08 13:35:59 -0700

    remove simlink

--- yourbased.sh
+++ yourbased.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get install -y tzdata
+apt-get update 
 gem install bundler -v 2.0.1
 apt-get -y install chromium-chromedriver
 # before_install
@@ -10,7 +10,7 @@ gem --version
 # install
 ruby -S bundle install
 # before_script
-ln -s /usr/lib/chromium-browser/chromedriver ~/bin/chromedriver
+# ln -s /usr/lib/chromium-browser/chromedriver /usr/bin/chromedriver
 google-chrome-stable --headless --disable-gpu --remote-debugging-port=9222 http://localhost &
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
 chmod +x ./cc-test-reporter
