953d6d5412dffaa1077acf5f2ca051f9b2b38cf7
wmuenigneer <adam.d.bell@icloud.io>
2019-08-08 12:40:50 -0700

    initial setup:

---
+++ yourbased.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -ex
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get install -y tzdata
+gem install bundler -v 2.0.1
+apt-get install chromium-chromedriver
+# before_install
+gem update --system
+gem --version
+# install
+ruby -S bundle install
+# before_script
+ln -s /usr/lib/chromium-browser/chromedriver ~/bin/chromedriver
+google-chrome-stable --headless --disable-gpu --remote-debugging-port=9222 http://localhost &
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+chmod +x ./cc-test-reporter
+./cc-test-reporter before-build
+# script
+bin/citest
